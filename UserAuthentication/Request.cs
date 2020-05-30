89615d23a6e013ab9c2e1fe3bdca5d5d8f556150
Rasmus Olsson <r.olsson96@gmail.com>
2020-05-30 14:34:07 +0200

    added Authentication worker

---
+++ UserAuthentication/Request.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAuthentication
+{
+
+    class Request
+    {
+        public static readonly string LOGIN = "LOGIN";
+        public static readonly string REGISTER = "REGISTER";
+        public static readonly string CHECKIN = "CHECKIN";
+
+        public string method;
+        public string id;
+        public Account account;
+    }
+}
