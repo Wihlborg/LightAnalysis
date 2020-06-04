0cb4bc8628a215f24551401058c97df93c66ee0e
Rasmus Olsson <r.olsson96@gmail.com>
2020-06-04 14:09:57 +0200

    added model classes for frontend

---
+++ Frontend/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Frontend
+{
+    class Account
+    {
+        public string email { get; set; }
+        public string pw { get; set; }
+        public bool isAdmin { get; set; }
+    }
+}
---
+++ Frontend/UserRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Frontend
+{
+    class UserRequest
+    {
+        public const string LOGIN = "LOGIN";
+        public const string REGISTER = "REGISTER";
+        public const string CHECKIN = "CHECKIN";
+        public const string LOGOUT = "LOGOUT";
+
+        public string method { get; set; }
+        public string id { get; set; }
+        public Account account { get; set; }
+    }
+}
