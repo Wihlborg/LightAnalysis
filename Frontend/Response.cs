2f3e0f522c7dd1b3df9a49570bd91d5914d91068
Liridon Smailji <33609344+datnod@users.noreply.github.com>
2020-06-06 16:26:45 +0200

    Manually added Response to Frontend

---
+++ Frontend/Response.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Frontend
+{
+    class Response
+    {
+        public string sessionId { get; set; }
+        public bool success { get; set; }
+        public string msg { get; set; }
+    }
+}
